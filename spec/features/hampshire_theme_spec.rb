fca855e09e2bee261f5d3df93cb91785848742e4
lizconlan <liz@mysociety.org>
2014-09-23 17:51:50 +0100

    Add a cucumber test for the Hampshire theme homepage

---
+++ spec/features/hampshire_theme_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+feature "Custom Hampshire pages" do
+  scenario "Visiting the home page within the Hampshire theme" do
+    VCR.use_cassette('planningalerts') do
+      visit address_applications_url(:host => "hampshire.127.0.0.1.xip.io:3000")
+    end
+
+    page.should have_content("Search for and compare the outcomes of planning applications in Hampshire.")
+    page.should have_content("Enter a postcode")
+    page.should have_content("Enter an address")
+    page.should have_content("or locate me automatically")
+  end
+end
