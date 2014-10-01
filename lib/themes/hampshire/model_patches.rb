721c2e924edcf11fd1cfccad75e58bbd5fa20665
lizconlan <liz@mysociety.org>
2014-10-01 11:48:14 +0100

    Basic search configuration

--- lib/themes/hampshire/model_patches.rb
+++ lib/themes/hampshire/model_patches.rb
@@ -3,6 +3,9 @@ Rails.configuration.to_prepare do
     define_index do
       indexes description
       indexes address
+      indexes suburb, :facet => true
+      indexes postcode
+      indexes authority(:full_name), :as => :authority, :facet => true
       has date_scraped
     end
   end
