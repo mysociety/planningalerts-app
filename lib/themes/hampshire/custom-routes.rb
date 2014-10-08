6f962e683d7bbbbb7deb5c6cc9a88613b8d55346
lizconlan <liz@mysociety.org>
2014-10-08 19:16:11 +0100

    Switch off routes we're not intending to support

--- lib/themes/hampshire/custom-routes.rb
+++ lib/themes/hampshire/custom-routes.rb
@@ -1,3 +1,31 @@
 Rails.application.routes.draw do
+  # include authority & reference number in applications page url
+  # (keep original route too)
   match '/applications/:id/:authority/:reference' => 'applications#show', :constraints => {:reference => /.*/}
+
+  # switch off routes not used by the theme
+  match 'faq', :to => 'static#error_404'
+  match 'getinvolved', :to => 'static#error_404'
+  match 'how_to_write_a_scraper', :to => 'static#error_404'
+  match 'how_to_lobby_your_local_council', :to => 'static#error_404'
+
+  match 'donate', :to => 'static#error_404'
+  match 'donate/:catchall', :to => 'static#error_404'
+
+  match 'api', :to => 'static#error_404'
+  match 'api/:catchall', :to => 'static#error_404'
+
+  match 'alerts', :to => 'static#error_404'
+  match 'alerts/:catchall', :to => 'static#error_404'
+  match 'alerts/:id/:verb', :to => 'static#error_404'
+
+  match 'comments', :to => 'static#error_404'
+  match 'comments/:catchall', :to => 'static#error_404'
+  match 'comments/:id/:verb', :to => 'static#error_404'
+
+  match 'atdis', :to => 'static#error_404'
+  match 'atdis/:catchall', :to => 'static#error_404'
+  match 'atdis/feed/:number/atdis/1.0/applications.json', :to => 'static#error_404'
+
+  match '/layar/getpoi', :to => 'static#error_404'
 end
