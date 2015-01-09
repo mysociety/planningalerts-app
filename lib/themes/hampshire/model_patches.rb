0e9cac745c09ced32bd2b90919735ce7565ab146
lizconlan <liz@mysociety.org>
2015-01-09 17:16:21 +0000

    Add a prefix to our generated search indexes

--- lib/themes/hampshire/model_patches.rb
+++ lib/themes/hampshire/model_patches.rb
@@ -2,10 +2,18 @@ Rails.configuration.to_prepare do
   Application.instance_eval do
     scope :in_past_week, where("date_received > ?", 7.days.ago)
     scope :recent, where("date_received >= ?", 14.days.ago)
+
+    def search_index_name
+      prefix = ""
+      if defined?(Configuration::SITE_PREFIX)
+        prefix = Configuration::SITE_PREFIX
+      end
+      "#{prefix}#{self.class.name.downcase}"
+    end
   end
 
   Application.class_eval do
-    define_index do
+    define_index(search_index_name) do
       indexes council_reference
       indexes description
       indexes address
